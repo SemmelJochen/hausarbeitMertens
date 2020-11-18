a70eda3a27c3d89089e0002a030a47870b8b5d32
Fluecke <Luecke.Felix@gmx.de>
2020-11-18 14:30:17 +0100

    create class Modelcontainer

---
+++ src/model/Modelcontainer.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Modelcontainer {
+
+	// think about it -> maybe Hashset?
+	private ArrayList<Person> persons;
+	
+	public Modelcontainer() {
+		this.persons = new ArrayList<Person>();
+	}
+}
