8f00306abc725302bf781ed6ee4634a1782c3c72
Josua <jokup@t-online.de>
2020-12-02 13:18:32 +0100

    minor bug fixed and added saveOnExit feature

--- src/view/views/MainWindow.java
+++ src/view/views/MainWindow.java
@@ -12,6 +12,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -27,59 +28,58 @@ import view.components.ObserverMenuItem;
 import controller.Controller;
 
 public class MainWindow extends JFrame {
-	
-	private JOptionPane exitDialog = new JOptionPane(
-		    "The only way to close this dialog is by\n"
-		    	    + "pressing one of the following buttons.\n"
-		    	    + "Do you understand?",
-		    	    JOptionPane.QUESTION_MESSAGE,
-		    	    JOptionPane.YES_NO_OPTION);
+
+	private JOptionPane exitDialog = new JOptionPane("The only way to close this dialog is by\n"
+			+ "pressing one of the following buttons.\n" + "Do you understand?", JOptionPane.QUESTION_MESSAGE,
+			JOptionPane.YES_NO_OPTION);
 
 	/**
 	 * Create the frame.
 	 */
 	public MainWindow() {
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		super();
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		this.setSize(new Dimension(1280, 720));
-		
-		//this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+
+		// this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
 		this.setJMenuBar(buildMenuBar());
-		this.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent winEvt) {
-				//TODO when closing the window, save the data
-				MainWindow.this.exitDialog.setVisible(true);
-			}
-		});
 	}
-	
+
 	public JMenuBar buildMenuBar() {
 		JMenuBar menuBar;
 		JMenu menu, submenu;
 		JMenuItem menuItem, subMenuItem;
 		ObserverMenuItem oMenuItem;
 
-		//Create the menu bar.
+		// Create the menu bar.
 		menuBar = new JMenuBar();
-		
-		//Build the first menu.
+
+		// Build the first menu.
 		menu = new JMenu("Ansicht");
 		menu.setMnemonic(KeyEvent.VK_A);
 		menuBar.add(menu);
 
-		//add Uebersicht menu
+		// add Uebersicht menu
 		submenu = new JMenu("Uebersicht");
 		subMenuItem = new JMenuItem("Studentenuebersicht");
 		submenu.add(subMenuItem);
 		subMenuItem = new JMenuItem("Erstgutachteruebersicht");
+		subMenuItem.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				MainWindow.this.setContentPane(new FirstReviewerOverview());
+			}
+		});
 		submenu.add(subMenuItem);
 		subMenuItem = new JMenuItem("Zweitgutachteruebersicht");
 		submenu.add(subMenuItem);
 		menu.add(submenu);
 
-		//add G-Einsicht
+		// add G-Einsicht
 		menuItem = new JMenuItem("Gutachtereinsicht");
 		menu.add(menuItem);
-		
+
 		submenu = new JMenu("Diagramme");
 		subMenuItem = new JMenuItem("Diagramm 1");
 		submenu.add(subMenuItem);
@@ -89,30 +89,36 @@ public class MainWindow extends JFrame {
 		submenu.add(subMenuItem);
 		menu.add(submenu);
 
-
-		//Build Edit menu in the menu bar.
+		// Build Edit menu in the menu bar.
 		menu = new JMenu("Edit");
 		menu.setMnemonic(KeyEvent.VK_N);
-		
-		//add edit action items 
+
+		// add edit action items
 		menuItem = new JMenuItem("Save");
-		menuItem.setAccelerator(KeyStroke.getKeyStroke(
-		        KeyEvent.VK_S, ActionEvent.META_MASK));
+		menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.META_MASK));
 		menu.add(menuItem);
 		menu.addSeparator();
-		oMenuItem = new ObserverMenuItem("Undo");  //Action Listener einfuegen
-		oMenuItem.setAccelerator(KeyStroke.getKeyStroke(
-		        KeyEvent.VK_Z, ActionEvent.META_MASK));
+		oMenuItem = new ObserverMenuItem("Undo"); // Action Listener einfuegen
+		oMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.META_MASK));
 		menu.add(oMenuItem);
-		oMenuItem = new ObserverMenuItem("Redo");  //Action Listener einfuegen
-		oMenuItem.setAccelerator(KeyStroke.getKeyStroke(
-		        KeyEvent.VK_Y, ActionEvent.META_MASK));
+		oMenuItem = new ObserverMenuItem("Redo"); // Action Listener einfuegen
+		oMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, ActionEvent.META_MASK));
 
 		menu.add(oMenuItem);
-		
+
 		menuBar.add(menu);
-	
+
 		return menuBar;
 	}
 	
+	public int showWarningMessage() {
+		String[] buttonLabels = new String[] { "Yes", "No", "Cancel" };
+		String defaultOption = buttonLabels[0];
+		Icon icon = null;
+
+		return JOptionPane.showOptionDialog(this,
+				"There's still something unsaved.\n" + "Do you want to save before exiting?", "Warning",
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE, icon, buttonLabels, defaultOption);
+	}
+
 }
