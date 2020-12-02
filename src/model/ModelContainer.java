c6f59c866bbb886a21488829ab2c826484632c1b
Fluecke <Luecke.Felix@gmx.de>
2020-12-02 10:33:31 +0100

    Auto stash before merge of "master" and "origin/master"
    settled Modelcontainer

--- src/model/ModelContainer.java
+++ src/model/ModelContainer.java
@@ -4,10 +4,18 @@ import java.util.HashMap;
 
 public class ModelContainer {
 
+	//Integer <- Hashcode
 	private HashMap<Integer, PeerReviewer> peerReviewer;
+	private HashMap<String, Integer> mappingPeerReviewerNameToKey;
 	
-	private HashMap<String, Integer> mappingNameToKey;
+	public ModelContainer() {
+		this.peerReviewer = new HashMap<Integer, PeerReviewer>();
+		this.mappingPeerReviewerNameToKey = new HashMap<String, Integer>();
+	}
 	
-	
-		
+	public void putPeerReviewer(PeerReviewer peerReviewer) {
+		int key = peerReviewer.hashCode();
+		this.peerReviewer.put(key, peerReviewer);
+		this.mappingPeerReviewerNameToKey.put(peerReviewer.getFirstName() + peerReviewer.getName(), key);
+	}
 }
