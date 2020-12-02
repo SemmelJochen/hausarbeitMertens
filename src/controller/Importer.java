cdcbf67ad797d50973ad2a46337f7818f02293a2
Fluecke <Luecke.Felix@gmx.de>
2020-12-02 12:33:13 +0100

    edited importer; TODO secondBachelorThesises

--- src/controller/Importer.java
+++ src/controller/Importer.java
@@ -3,53 +3,82 @@ package controller;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 
+import javax.management.openmbean.KeyAlreadyExistsException;
 import javax.swing.JFileChooser;
+
 import model.ModelContainer;
 import model.PeerReviewer;
-import model.Person;
 import model.Student;
 
 public class Importer {
 
 	private JFileChooser fileChooser;
-	private ModelContainer model;
 
 	public Importer() {
 		this.fileChooser = new JFileChooser();
-		this.model = new ModelContainer();
 	}
-	
+
 	public void chooseFile() {
 		this.fileChooser.showOpenDialog(null);
 	}
 	
-	public void work() {
+	/*
+	 * the parameter modelContainer is changed after running the method!
+	 */
+	public void importCsvInModelContainer(ModelContainer modelContainer) {
 		this.chooseFile();
 		File file = this.fileChooser.getSelectedFile();
-		
+
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-		    String line;
-		    while ((line = br.readLine()) != null) {
-		       if(line.contains("/")) {
-		    	   String[] entries = line.split(";");
-		    	   String[] names = entries[0].split(", ");
-		    	   Person student = new Student(names[1], names[0], entries[1], "email", entries[2], entries[3]);
-		    	   System.out.println(student);
-		    	   System.out.println("");
-		       }
-		    }
+			int i = 0;
+			String line = br.readLine();
+			while (line != null) {
+				if (i > 3) {
+					Student student = this.newStudent(line);
+					PeerReviewer peerReviewer = this.newPeerReviewer(line);
+					peerReviewer.addFirstBachelorThesis(student);
+
+					modelContainer.putPeerReviewer(peerReviewer);
+				} else {
+					i++;
+				}
+				line = br.readLine();
+			}
+		} catch (Exception e) {
+			System.err.println(e);
 		}
-		catch(Exception e) {
-			
-		}
-		
 	}
 
-	
-	
+	public Student newStudent(String line) {
+		String[] entries = line.split(";");
+		String[] names = entries[0].split(", ");
+		return new Student(names[1], names[0], "", entries[1], entries[2], entries[3]);
+	}
+
+	public PeerReviewer newPeerReviewer(String line) {
+		String[] entries = line.split(";");
+		String[] peerReviewerString = entries[4].split(" ");
+		if (peerReviewerString[0].equals("Dr.")) {
+			return new PeerReviewer(peerReviewerString[0], peerReviewerString[3], peerReviewerString[2], "", -1);
+		}
+		if (peerReviewerString[0].equals("Prof.")) {
+			return new PeerReviewer(peerReviewerString[0] + " " + peerReviewerString[1], peerReviewerString[3],
+					peerReviewerString[2], "", -1);
+		}
+
+		return new PeerReviewer("", peerReviewerString[1], peerReviewerString[0], "", -1);
+	}
+
 	public static void main(String[] args) {
 		Importer imp = new Importer();
-		imp.work();
+		ModelContainer m = new ModelContainer();
+		imp.importCsvInModelContainer(m);
+		m.printPeerReviewers();
+		
 	}
 }
