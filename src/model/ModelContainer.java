937a3b9512d4898991520e4a17a320d018867c98
Fluecke <Luecke.Felix@gmx.de>
2020-12-02 12:32:20 +0100

    refactor Modelcontainer

--- src/model/ModelContainer.java
+++ src/model/ModelContainer.java
@@ -1,21 +1,37 @@
 package model;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class ModelContainer {
 
-	//Integer <- Hashcode
-	private HashMap<Integer, PeerReviewer> peerReviewer;
-	private HashMap<String, Integer> mappingPeerReviewerNameToKey;
+	private HashMap<String, PeerReviewer> peerReviewers;
 	
 	public ModelContainer() {
-		this.peerReviewer = new HashMap<Integer, PeerReviewer>();
-		this.mappingPeerReviewerNameToKey = new HashMap<String, Integer>();
+		this.peerReviewers = new HashMap<String, PeerReviewer>();
 	}
 	
 	public void putPeerReviewer(PeerReviewer peerReviewer) {
-		int key = peerReviewer.hashCode();
-		this.peerReviewer.put(key, peerReviewer);
-		this.mappingPeerReviewerNameToKey.put(peerReviewer.getFirstName() + peerReviewer.getName(), key);
+		String key = peerReviewer.getFirstName() + peerReviewer.getName();
+		PeerReviewer existingPeerReviewer = this.peerReviewers.get(key);
+		
+		if(existingPeerReviewer != null) {
+			existingPeerReviewer.addFirstBachelorThesis(peerReviewer.getFirstBachelorThesis());
+			existingPeerReviewer.addSecondBachelorThesis(peerReviewer.getSecondBachelorThesis());
+		}else {
+			this.peerReviewers.put(key, peerReviewer);
+		}
+	}
+	
+	public void printPeerReviewers() {
+		Iterator it = peerReviewers.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry) it.next();
+			System.out.println(pair.getKey() + "\n" + pair.getValue());
+			((PeerReviewer) pair.getValue()).printStudents();
+			System.out.println("");
+			it.remove(); // avoids a ConcurrentModificationException
+		}
 	}
 }
