51d63a160d6ba15abce36215e17e763f1e9d3e08
Josua <jokup@t-online.de>
2020-12-05 12:49:46 +0100

    improved the ContentPane

--- src/view/components/ContentPane.java
+++ src/view/components/ContentPane.java
@@ -6,42 +6,35 @@ import java.awt.Component;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
-import view.views.MainWindow;
-
 public class ContentPane extends JPanel {
-	
+
 	private JPanel header = new JPanel();
 	private JPanel content = new JPanel();
-	private MainWindow mainWindow;
-	
-	//constructor for initialize the component with no content
-	public ContentPane() {
-		super();
-		//call null constructor
-		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		
-		this.add(this.header, BorderLayout.PAGE_START);
-		this.add(this.content, BorderLayout.CENTER);
-	}
-	
-	//constructor for initialize component with content
-	public ContentPane(Component headerComponent, Component contentComponent) {
 
+	// constructor for initialize the component with no content
+	public ContentPane() {
+		//create new window with no content (empty panel)
+		this(new JPanel(), new JPanel());
+	}
+
+	// constructor for initialize component with content
+	public ContentPane(Component headerComponent, Component contentComponent) {
+		super();
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		
+
 		this.setHeader(headerComponent);
 		this.setContent(contentComponent);
-		
+
 		this.add(this.header, BorderLayout.PAGE_START);
 		this.add(this.content, BorderLayout.CENTER);
-	
+
 	}
-	
+
 	public void setContent(Component c) {
 		this.content.removeAll();
 		this.content.add(c);
 	}
-	
+
 	public void setHeader(Component c) {
 		this.header.removeAll();
 		this.header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
