92006897f61c2b778c87d9a12d38d6125675b059
Fluecke <Luecke.Felix@gmx.de>
2020-12-11 14:06:14 +0100

    added PeerReviewers

--- src/model/Student.java
+++ src/model/Student.java
@@ -9,13 +9,40 @@ public class Student extends Person implements Serializable {
 
 	private static final long serialVersionUID = 0xBAFFL;
 	private String studentGroup, practicePartner, subject;
+	private PeerReviewer firstPeerReviewer;
+	private PeerReviewer secondPeerReviewer;
 
+	public Student(String name, String firstName, String email, String studentGroup, String practicePartner,
+			String subject, PeerReviewer firstPeerReviewer, PeerReviewer secondPeerReviewer) {
+		super(name, firstName, email);
+		this.studentGroup = studentGroup;
+		this.practicePartner = practicePartner;
+		this.subject = subject;
+		this.firstPeerReviewer = firstPeerReviewer;
+		this.secondPeerReviewer = secondPeerReviewer;
+	}
 	public Student(String name, String firstName, String email, String studentGroup, String practicePartner,
 			String subject) {
 		super(name, firstName, email);
 		this.studentGroup = studentGroup;
 		this.practicePartner = practicePartner;
 		this.subject = subject;
+	}
+
+	public PeerReviewer getFirstPeerReviewer() {
+		return this.firstPeerReviewer;
+	}
+
+	public void setFirstPeerReviewer(PeerReviewer firstPeerReviewer) {
+		this.firstPeerReviewer = firstPeerReviewer;
+	}
+
+	public PeerReviewer getSecondPeerReviewer() {
+		return this.secondPeerReviewer;
+	}
+
+	public void setSecondPeerReviewer(PeerReviewer secondPeerReviewer) {
+		this.secondPeerReviewer = secondPeerReviewer;
 	}
 
 	public String getStudentGroup() {
