ddb9fd5020244fb75a7572105e4e56bbf360e510
Fluecke <Luecke.Felix@gmx.de>
2020-12-11 14:27:45 +0100

    added legend

--- src/view/views/DiagramOverview.java
+++ src/view/views/DiagramOverview.java
@@ -1,6 +1,8 @@
 package view.views;
 
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import view.components.ContentPane;
 import view.components.PieChart;
@@ -8,12 +10,24 @@ import view.components.PieChart;
 public class DiagramOverview extends ContentPane {
 
 	private static final long serialVersionUID = 57267L;
+	PieChart pieChart;
 	
 	public DiagramOverview() {
 		super();
-		PieChart pieChart = new PieChart();
-		
+		this.pieChart = new PieChart();
+		JPanel content = new JPanel();
+		content.add(this.buildLegend());
+		content.add(pieChart);
 		this.setHeader(new JLabel("Anzahl der Bachelorthesen eines Dozenten relativ zu allen Thesen"));
-		this.setContent(pieChart);
+		this.setContent(content);
+	}
+	
+	public JPanel buildLegend() {
+		JPanel legend = new JPanel();
+		legend.setLayout(new BoxLayout(legend, BoxLayout.Y_AXIS));
+		for(String peerReviewerString: this.pieChart.getSlices().values()) {
+			legend.add(new JLabel(peerReviewerString));
+		}
+		return legend;
 	}
 }
